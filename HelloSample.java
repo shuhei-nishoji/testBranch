18a750ec166d63cd0806e2a6805389153212186c
shuhei-nishoji <s.nishoji1811@morijyobi.ac.jp>
2019-05-31 11:12:58 +0900

    [modify]指摘の修正

--- HelloSample.java
+++ HelloSample.java
@@ -2,6 +2,7 @@
 public class HelloSample {
 	public static void main(String args[]){
 		System.out.println("Hello World");
+		System.out.println("おはようございます");
 		System.out.println("こんにちは");
 		System.out.println("さようなら");
 	}
